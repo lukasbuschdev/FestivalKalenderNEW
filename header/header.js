0aab5db93c83392dfc35ae7089c9a0a2b1b24dfd
Lukas Busch <lukas.busch1996@gmx.at>
2023-12-27 19:43:20 +0100

    +fetched database, fixed header

--- header/header.js
+++ header/header.js
@@ -5,7 +5,7 @@ function loadHeader() {
         <header class="bg-white flex-center">
             <div id="header-img">
                 <h1>Festivalkalender</h1>
-                <div id="search-bar row">
+                <div id="search-bar" class="row">
                     <img src="/assets/icons/loupe.svg">
                     <input type="text">
                 </div>
--- index/script.js
+++ index/script.js
@@ -1,4 +1,11 @@
-function init() {
+async function init() {
     loadHeader();
     loadFooter();
+    await getData();
+}
+
+async function getData() {
+    const url = '/database.json';
+    const data = await (await fetch(url)).json();
+    return data;
 }
