5e427a7ee1a6a3160def36665a870fb0938921a2
lukasbuschdev <lukas.busch1996@gmx.at>
2024-01-02 18:34:45 +0100

    +debounce implementation /header-search

--- header/header.js
+++ header/header.js
@@ -17,23 +17,25 @@ function loadHeader() {
     includeTemplate(headerContainer, headerContent);
 };
 
-document.addEventListener('DOMContentLoaded', function() {  
-    setTimeout(() => {
-        const inputField = $('#header-img input');
-        inputField.addEventListener('input', function() {
-            filterAndSearch();
-        });
-    }, 1000)
-});
+// async function filterAndSearch() {
+//     const input = $('#header-img input').value.toLowerCase();
+//     const filteredFestivals = (await dataSet()).festivals.filter(festival => {
+//         return festival.name.toLowerCase().includes(input) ||
+//                festival.location.toLowerCase().includes(input) ||
+//                festival.date.toLowerCase().includes(input) ||
+//                festival.genre.toLowerCase().includes(input);
+//     });
+
+//     search(filteredFestivals);
+// }
 
 async function filterAndSearch() {
     const input = $('#header-img input').value.toLowerCase();
-    const filteredFestivals = (await dataSet()).festivals.filter(festival => {
-        return festival.name.toLowerCase().includes(input) ||
-               festival.location.toLowerCase().includes(input) ||
-               festival.date.toLowerCase().includes(input) ||
-               festival.genre.toLowerCase().includes(input);
-    });
+    const festivals = (await dataSet()).festivals;
+  
+    const filteredFestivals = (await festivals).filter(({name, location, date, genre}) => 
+        [name, location, date, genre].some(attr => attr.toLowerCase().includes(input))
+    );
 
     search(filteredFestivals);
 }
@@ -42,3 +44,26 @@ function search(filteredData) {
     log(filteredData);
     loadFilteredEventCards(filteredData);
 }
+
+function debounce(func, delay) {
+    let debounceTimer;
+    return function() {
+        const context = this;
+        const args = arguments;
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(() => func.apply(context, args), delay);
+    };
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(() => {
+        const inputField = $('#header-img input');
+        
+        if(!inputField) return error("Input field not found");
+        if (inputField) {
+            inputField.addEventListener('input', debounce(function() {
+                filterAndSearch();
+            }, 250));
+        }
+    }, 250);
+});
